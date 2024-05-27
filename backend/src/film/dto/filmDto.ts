0cec3eafe2e5039aaa2ee4fd19ced6a9cfa6ff27
l1agosta <goodtimeszxc@gmail.com>
2024-05-27 18:23:19 +0500

    added dto for film

---
+++ backend/src/film/dto/filmDto.ts
@@ -0,0 +1,52 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class FilmDto {
+
+  @ApiProperty({
+    description: 'Название фильма',
+    example: 'Час пик',
+  })
+  readonly title: string;
+
+  @ApiProperty({
+    description: 'Описание фильма',
+    example: 'Крутой фильм!!!',
+  })
+  readonly description: string;
+
+  @ApiProperty({
+    description: 'Рейтинг фильма',
+    example: '4.7',
+  })
+  readonly rating: number;
+
+  @ApiProperty({
+    description: 'Год выпуска фильма',
+    example: '2017',
+  })
+  readonly year: number;
+
+  @ApiProperty({
+    description: 'Длительность фильма',
+    example: '195',
+  })
+  readonly time: number;
+
+  @ApiProperty({
+    description: 'URL на сторонний API, где хрнаится картинка фильма',
+    example: 'i.bb/....',
+  })
+  readonly avatar: string;
+
+  @ApiProperty({
+    description: 'Бюджет фильма',
+    example: '195 111',
+  })
+  readonly budget: number;
+
+  @ApiProperty({
+    description: 'Выручка фильма',
+    example: '2 154 661',
+  })
+  readonly revenue: number;
+}
