9120c2d496bef589c50a7d57f442a6185c63fee2
l1agosta <goodtimeszxc@gmail.com>
2024-06-07 23:17:48 +0500

    fixed film card

--- frontend/src/ui/FilmCard/FilmCard.tsx
+++ frontend/src/ui/FilmCard/FilmCard.tsx
@@ -3,6 +3,7 @@ import './FilmCard.css';
 export function FilmCard(){
     return (
         <div className="film-card">
+            <span className="top-film-card">1</span>
             <img src="https://www.kino-teatr.ru/movie/poster/133117/96180.jpg" alt=""/>
         </div>
     )
