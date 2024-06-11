517e18fe38a6e3fbe0c88bc1f33f80d4af852c69
l1agosta <goodtimeszxc@gmail.com>
2024-06-12 01:15:57 +0500

    added some design on profile page

--- frontend/src/components/Profile/Favorites/Favorites.tsx
+++ frontend/src/components/Profile/Favorites/Favorites.tsx
@@ -1,9 +1,9 @@
 import {useEffect} from "react";
 import {observer} from "mobx-react-lite";
 import users from "../../../store/users.ts";
-import {film} from "../../../interfaces/film.ts";
-import {FilmCard} from "../../../ui/FilmCard/FilmCard.tsx";
 import uniqid from "uniqid";
+import {FilmCard} from "../../../ui/FilmCard/FilmCard.tsx";
+import {film} from "../../../interfaces/film.ts";
 
 export const Favorites = observer(() => {
 
@@ -15,17 +15,25 @@ export const Favorites = observer(() => {
 
     return (
         <div className="d-flex gap-5 flex-wrap">
-            {users.fullUserData.favorites.map((item: film, index: number) => {
-                return (
-                    <FilmCard
-                        image={item.image}
-                        title={item.title}
-                        id={item.id}
-                        key={uniqid()}
-                        index={index}
-                    />
-                );
-            })}
+            {users.fullUserData.favorites.length === 0 ? (
+                <h2>Избранных фильмов пока нет</h2>
+            ) : (
+                <>
+                    {users.fullUserData.favorites.map((item: film, index: number) => {
+                        return (
+                            <FilmCard
+                                image={item.image}
+                                title={item.title}
+                                id={item.id}
+                                key={uniqid()}
+                                index={index}
+                            />
+                        );
+                    })}
+                </>
+            )}
+
+
         </div>
     );
 });
